c516d8f073486d048060187a60a6e2f342eadc87
Azizjan Ayupov <litpulla@mail.ru>
2021-01-21 22:50:28 +0600

    temp

--- app/src/main/java/com/example/timeleftpercent/ExampleService.java
+++ app/src/main/java/com/example/timeleftpercent/ExampleService.java
@@ -3,7 +3,11 @@ package com.example.timeleftpercent;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -12,7 +16,8 @@ import androidx.core.app.NotificationCompat;
 import static com.example.timeleftpercent.App.CHANNEL_ID;
 
 public class ExampleService extends Service {
-
+    final Handler handler=new Handler();
+    Intent widgetUpdateIntent;
     @Override
     public void onCreate() {
         super.onCreate();
@@ -34,9 +39,37 @@ public class ExampleService extends Service {
         Notification notification = notificationBuilder.build();
 
         startForeground(1, notification);
+
+
+        // for widget update
+        widgetUpdateIntent = new Intent(this, NewAppWidget.class);
+        widgetUpdateIntent.setAction("someFunc");
+        // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+        // since it seems the onUpdate() is only fired on that:
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplication());
+        int[] ids = appWidgetManager.getAppWidgetIds(new ComponentName(getApplication(), NewAppWidget.class));
+        widgetUpdateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        // widgetUpdateIntent.putExtra("text", "input");
+        sendBroadcast(widgetUpdateIntent);
+
         return START_REDELIVER_INTENT;
     }
 
+    final Runnable updateTask=new Runnable() {
+        @Override
+        public void run() {
+            // for widget update
+            widgetUpdateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            // Use an array and EXTRA_APPWIDGET_IDS instead of AppWidgetManager.EXTRA_APPWIDGET_ID,
+            // since it seems the onUpdate() is only fired on that:
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(getApplication());
+            int[] ids = appWidgetManager.getAppWidgetIds(new ComponentName(getApplication(), NewAppWidget.class));
+            widgetUpdateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+           // widgetUpdateIntent.putExtra("text", "input");
+            sendBroadcast(widgetUpdateIntent);
+            handler.postDelayed(this,1000);
+        }
+    };
     @Override
     public void onDestroy() {
         super.onDestroy();
--- app/src/main/java/com/example/timeleftpercent/MainActivity.java
+++ app/src/main/java/com/example/timeleftpercent/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.timeleftpercent;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         serviceIntent.putExtra("inputExtra", input);
 
         startForegroundService(serviceIntent);
+
+
     }
 
     public void stopService(View v){
